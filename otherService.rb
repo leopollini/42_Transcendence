656692d48c7fdf8edc85014fff191ff84ac17b71
SuperForceLeo <leopollinik@gmail.com>
2024-12-02 16:59:27 +0100

    lol"

--- otherService.rb
+++ otherService.rb
@@ -1,6 +1,28 @@
-require 'socket'
+# main.rb
 
-s = TCPSocket.new "localhost", 8080
-s.puts("asjhdkahsjd")
-s.close
-sleep 2
+# require 'timeout'
+require 'json'
+
+load ((File.file? '/var/www/common/Ports.rb') ? '/var/www/common/Ports.rb' : '../common_tools/tools/Ports.rb')
+
+load ((File.file? '/var/www/common/RequestUnpacker.rb') ? '/var/www/common/RequestUnpacker.rb' : '../common_tools/tools/RequestUnpacker.rb')
+
+
+load ((File.file? '/var/www/common/BetterPG.rb') ? '/var/www/common/BetterPG.rb' : '../common_tools/tools/BetterPG.rb')
+
+
+$stdout.sync = true
+SERVICE_NAME = "sample_service"
+
+USR = BetterPG::SimplePG.new "users", ["id INT", "username TEXT", "password TEXT", "token TEXT"]
+
+def sample_service_loop(client, server)
+
+end
+
+PORT = PortFinder::FindPort.new(SERVICE_NAME).getPort
+
+print "lolresponse active at port ", PORT, "\n"
+(SimpleServer::SimplerTCP.new PORT, :sample_service_loop).start_loop
+
+
