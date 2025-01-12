b8a668f852f73f7d5d9274b34af5ab662a3d9ee2
SuperForceLeo <leopollinik@gmail.com>
2025-01-12 18:37:44 +0100

    game_manager service working

--- srcs/game_manager/Rooms.rb
+++ srcs/game_manager/Rooms.rb
@@ -1,10 +1,12 @@
 
 module Code
-	ENTER_AS_PLAYER1 = 1
-	ENTER_AS_PLAYER2 = 2
+	SUCCESS = 1
+  RECONNECTED = 2
 	NEW_ROOM_CREATED = 3 # room creator is always player 1
 
 	DUPLICATE_USERNAME = 4
+  TOURNAMENT_ROOM_NOT_CREATED = 6
+  RECONNECTION_FAILED = 5
 
 	FAILED = 0
 end
@@ -12,8 +14,8 @@ end
 module Status
 #post-connection
 	P1_STATUS = 1
-	P2_STATUS = 2			# 0: playing, -1: not paired, 1: disconnected
-	IN_PROGRESS = 3
+	P2_STATUS = 2			# 0: playing, -1: not paired, 1: disconnected/paired
+	IN_PROGRESS = 3   # 0: in progress, -1: waiting for players, 1: finished
 	PAUSED = 4
 	
 	BROKEN = 0
@@ -21,18 +23,18 @@ end
 
 class Room
 	@@ids = 0
-	def initialize(p1 = "", p2 = "", id)
-		raise "Room cretor: invalid input" if p1.nil? || p2.nil?
+	def initialize(p1, p2 = "", id)
+		raise "Room cretor: invalid input" if p1.nil? || p2.nil? || p1.empty?
 		@player1 = p1.to_s
 		@player2 = p2.to_s
 		@socket_p1 = nil
 		@socket_p2 = nil
-		@status = [1, 1, false, false, false]
+		@status = [false, 1, p2.empty? ? -1 : 1, -1, false]
 		@updated = Time.now.to_i
 		@mutex = Mutex.new
 		@id = @@ids
 		@@ids = @@ids + 1
-	puts "Created new room for " + p1 +"; id: " + @id.to_s
+	  puts "Created new room for " + p1 + (p2.empty? ? "" : (" and " + p2)) + "; id: " + @id.to_s
 	end
 	def status(s = -1)
 		if s != -1
@@ -40,6 +42,43 @@ class Room
 		end
 		return @status
 	end
+  def disconnected(playername, other = nil)
+    @mutex.synchronize do
+      p = amIP1 playername
+      @status[p ? Status::P1_STATUS : Status::P2_STATUS] = 1
+      other.puts '{"status":"opponent_disconnected","success":"false","paused":"false"}' if other && !other.closed?
+      if @status[!p ? Status::P1_STATUS : Status::P2_STATUS] != 0
+        puts "Room is now empty!!! Pausing game"
+        @status[Status::PAUSED] = true
+      end
+    end
+  end
+  def reconnected(playername, client)
+    @mutex.synchronize do
+      p = amIP1 playername
+      @status[p ? Status::P1_STATUS : Status::P2_STATUS] = 0
+      msg = '{"opponent":"' + getOpponent(playername) + '""status":"reconnected","success":"true"'
+      if @status[!p ? Status::P1_STATUS : Status::P2_STATUS] != 0
+        msg << ',"paused":"true"'
+      puts "game is paused because of missing player"
+      else
+        msg << ',"pauesd":"false"'
+      puts "game is unpaused because all players are here!"
+        @status[Status::PAUSED] = false
+      end
+      msg << '}'
+      client.puts msg
+    end
+  end
+	def getOpponent(playername)
+		return (playername == @player1 ? @player2 : @player1)
+  end
+	def getOpponentSocket(clientsock)
+		return (clientsock == @socket_p1 ? @socket_p2 : @socket_p1)
+  end
+  def amIP1(playername)
+    return playername == @player1 ? true : false
+  end
 	def setPlayer2(playername)
 		@player2 = playername.to_s
 	end
@@ -51,7 +90,7 @@ class Room
 			@socket_p1 = sock if playername == @player1
 			@socket_p2 = sock if playername == @player2
 		end
-	return [@socket_p1, @socket_p2]
+	  return [@socket_p1, @socket_p2]
 	end
 	def getSockets()
 		return [@socket_p1, @socket_p2]
@@ -73,47 +112,101 @@ end
 class Rooms_lst
 	def initialize()
 		@mutex = Mutex.new
-		@rooms = []
+		@waiting_rooms = []
+    @assigned_rooms = []
     @active_rooms = []
-	end
-  def addToActive(room)
+    @tournament_rooms = []
+  end
+  def activateRoom(room)
+    raise "orcam erda" if room.nil?
+    return if @active_rooms.include? room
+    raise "room not assigned" if !@assigned_rooms.include? room
     @mutex.synchronize do
+      @assigned_rooms.delete room
       @active_rooms.append room
     end
+    return room
   end
-	def purgeRooms()
-    @mutex.synchronize do
-      t = @rooms.clone
-      t.each do | r |
-        if Time.now.to_i > r.getLastUpdate + ROOM_LIFETIME
-          puts "Deleting room " + r.getId.to_s
-          @rooms.delete r
-        end
-      end
+  def moveRoom(room, to, from = nil)
+    raise "orcam erda" if room.nil?
+    from.delete room if from
+    to.append room if room
+    return room
+  end
+	def purge(room = nil)
+    return if room.nil?
+    @waiting_rooms.delete room if @waiting_rooms.includes? room
+    [@waiting_rooms, @assigned_rooms, @active_rooms, @tournament_rooms].each do | a |
+      next if !a.include? room
+
     end
 	end
-	def please(client, playername = "", opponent = "")	# random opponent, tournament mode or specific opponent
+  def findYourRoom(room, playername, opponent)
+    if room.getPlayers()[0] == playername && (opponent.empty? || room.getPlayers()[1] == opponent)
+      return [room, Code::DUPLICATE_USERNAME] if room.getStatus[Status::P1_STATUS] == 0
+      return [room, Code::SUCCESS]
+    end
+    if room.getPlayers()[1] == playername && (opponent.empty? || room.getPlayers()[0] == opponent)
+      return [room, Code::DUPLICATE_USERNAME] if room.getStatus[Status::P2_STATUS] == 0
+      return [room, Code::SUCCESS]
+    end
+    return nil
+  end
+  def getRoomsCount(include_tournament = true)
+    return @waiting_rooms.count + @assigned_rooms.count + @active_rooms.count + (include_tournament ? @tournament_rooms.count : 0)
+  end
+	def please(info, client, playername = "", opponent = "")	# random opponent, tournament mode or specific opponent
 		@mutex.synchronize do
 			# purgeRooms
-			(@active_rooms + @rooms).each do | room | # SHOULD check active rooms before free roms
-				if room.getPlayers()[0] == playername && (opponent.empty? || room.getPlayers()[1] == opponent)
-					return [room, Code::DUPLICATE_USERNAME] if room.getStatus[Status::P1_STATUS] == 0
-					return [room, Code::ENTER_AS_PLAYER1]
-				end
-				if room.getPlayers()[1] == playername && (opponent.empty? || room.getPlayers()[0] == opponent)
-					return [room, Code::DUPLICATE_USERNAME] if room.getStatus[Status::P2_STATUS] == 0
-					return [room, Code::ENTER_AS_PLAYER2]
-				end
+
+# if the player is reconnecting
+      # if info["reconnecting"] == "true"
+        @active_rooms.each do | room |
+          t = findYourRoom room, playername, opponent
+          if t
+            room.reconnected playername, client
+          puts "Found in active rooms"
+            return [t[0], t[1] == Code::SUCCESS ? Code::RECONNECTED : t[1]]
+          end
+        end
+        return [nil, Code::RECONNECTION_FAILED] if info["reconnecting"] == "true" # in case room is not found
+      # end
+# if the player is looking for his tournament room (opponent may be specified)
+      if info["tournament"] == "true"
+			@tournament_rooms.each do | room |
+          t = findYourRoom room, playername, opponent
+          moveRoom room, @assigned_rooms if t
+          return t if t
+        end
+        return [nil, Code::TOURNAMENT_ROOM_NOT_CREATED]  # in case room is not found
+      end
+# if the player is already matched
+			@assigned_rooms.each do | room |
+        t = findYourRoom room, playername, opponent
+        puts "room found? : " + t.class.to_s
+        return t if !t.nil?
 			end
-			@rooms.each do | room | # look for random free waiting room
-        if room.getPlayers()[1] == "" && (opponent == "" )
+# if the player is looking for a random game
+			@waiting_rooms.each do | room | # look for random free waiting room
+        if room.getPlayers()[0] == playername
+          puts "sent to his room"
+          return [room, Code::DUPLICATE_USERNAME] if room.getStatus[Status::P1_STATUS] == 0
+          return [room, Code::SUCCESS]
+        end
+        if room.getPlayers()[1] == "" && (opponent == "") && room.getPlayers()[0] != playername
           room.setPlayer2 playername
-          return [room, Code::ENTER_AS_PLAYER2]
+        puts "inserted in free room"
+          moveRoom room, @assigned_rooms, @waiting_rooms
+          return [room, Code::SUCCESS]
         end
       end
-			# reaches this if room for player does not exist
+# reaches this if room for player does not exist
 			room = Room.new playername, opponent
-			@rooms.append room
+      if opponent.empty?
+        @waiting_rooms.append room
+      else
+        @assigned_rooms.apend room
+      end
 			return [room, Code::NEW_ROOM_CREATED]
 		end
 	end
