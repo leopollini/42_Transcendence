613dd549a2f990ca4b789b92cb4db3329a583c0b
Samir Ibrahim <samir.ibrahim@outlook.it>
2025-04-05 10:37:08 +0200

    pong game customizations preview now works with different backgrounds set

--- srcs/trascendence/public/website/js/game/pong/scene/background.js
+++ srcs/trascendence/public/website/js/game/pong/scene/background.js
@@ -9,10 +9,6 @@ export function createStarsBackground(game, count) {
 
 export function renderBackground(game) {
     // Walls
-    if (background == "space")
-        game.ctx.fillStyle = game.wallsColor;
-    else 
-        game.ctx.fillStyle = "white";
 
     if (background == "pingpong") {
         // Background
@@ -24,13 +20,15 @@ export function renderBackground(game) {
     }
 
     if (background == "classic") {
+        game.ctx.fillStyle = game.wallsColor;
         // Draw vertical dashed line
         for (let y = game.wallThickness; y < game.canvas.height - game.wallThickness; y += 50) {
             game.ctx.fillRect(game.canvas.width / 2, y, game.wallThickness, 30);
         }
     }
+    game.ctx.fillStyle = game.wallsColor;
     game.ctx.shadowColor = game.wallsColor;
-    game.ctx.shadowBlur = 20;
+    game.ctx.shadowBlur = 10;
     game.ctx.fillRect(game.wallThickness, 0, game.canvas.width - game.wallThickness, game.wallThickness);
     game.ctx.fillRect(game.wallThickness, game.canvas.height - game.wallThickness, game.canvas.width - game.wallThickness, game.wallThickness);
     game.ctx.fillRect(0, 0, game.wallThickness, game.canvas.height);
