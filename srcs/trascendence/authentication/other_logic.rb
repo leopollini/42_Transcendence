f78f5fb5dd0782c60a4c87bc1111cab1ffd542d4
SamueleGalli <samuele.aggeggio@gmail.com>
2025-04-06 17:47:16 +0200

    final_push

--- srcs/trascendence/authentication/other_logic.rb
+++ srcs/trascendence/authentication/other_logic.rb
@@ -41,20 +41,6 @@ module Other_logic
       },
       do_create: true
     }
-    puts "adding token cookie"
-    #response.body = SimpleServer.method_req("login_user", payload)
-    #puts response
-    #data = JSON.parse(response)
-    #if data["token"]
-    #  token = data["token"]
-    #  
-    #  cookie = WEBrick::Cookie.new("logged_token", token)
-    #  
-    #  cookie.secure = true 
-    #  cookie.http_only = true
-    #  cookie.same_site = 'Strict'
-    #
-    #  res.cookies << cookie
-    #end
+    #SimpleServer.method_req("login_user", payload)
   end
 end
