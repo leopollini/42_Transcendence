2ae68bfb4907fa990f61e137c81b378b5d621ac3
SamueleGalli <samuele.aggeggio@gmail.com>
2025-04-07 15:11:52 +0200

    little improvements

--- setup/setup_online_website.sh
+++ setup/setup_online_website.sh
@@ -5,6 +5,7 @@ GREEN='\e[32m'
 YELLOW='\e[33m'
 BLUE='\e[34m'
 RESET='\e[0m'  
+CERT_DIR="./srcs/trascendence/authentication/ssl_certs"
 
 if ! command -v ufw &> /dev/null
 then
@@ -30,4 +31,17 @@ else
 fi
 
 echo -e "\n${GREEN}Firewall configurato con successo\n${RESET}"
+
+if [ -d "$CERT_DIR" ]; then
+    rm -rf "$CERT_DIR"
+fi
+
+echo -e "${GREEN}Creating SSL folder...${RESET}"
+mkdir -p "$CERT_DIR"
+
+openssl req -new -x509 -days 365 -nodes \
+  -out "$CERT_DIR/server.crt" \
+  -keyout "$CERT_DIR/server.key" \
+  -subj "/C=IT/ST=F/L=MyCity/O=MyOrg/OU=MyDept/CN=localhost"
+
 sudo ufw status verbose
