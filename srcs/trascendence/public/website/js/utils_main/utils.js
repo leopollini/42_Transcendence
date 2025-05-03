9987e3cfeef31a0b50c5db1b4289a2ed90e30023
SamueleGalli <80041753+SamueleGalli@users.noreply.github.com>
2025-05-03 11:53:39 +0200

    Update utils.js

--- srcs/trascendence/public/website/js/utils_main/utils.js
+++ srcs/trascendence/public/website/js/utils_main/utils.js
@@ -32,9 +32,11 @@ export async function util_main(path, component, app) {
 
     if (path !== "/classic/lobby" && path !== "/classic")
         save_global("lobby_data", null);
-    //console.log("lobby_data", lobby_data);
-    /*if (window.location.pathname !== "/" && acess === true) 
-        save_global("user", await restore_user());*/
+    if (path === "/modes")
+    {
+        change_name(current_user.display_name);
+        update_image(current_user.image);
+    }
     if (/^Robin[4-8]$/.test(buttonTitle) || /^Bracket(4|8|16)$/.test(buttonTitle))
         save_global("numP", parseInt(buttonTitle.replace(/\D/g, ""), 10));
     if (!players)
